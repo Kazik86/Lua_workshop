f2c4a9d17212384be0db66dfdeac7dbeea15e65c
mtrzeciak <michal.trzeciak@vivaldi.net>
2015-06-10 18:43:02 +0200

    vim: open files from repository

--- vim.sh
+++ vim.sh
@@ -1,1 +1,1 @@
-gvim --cmd "set exrc" $(find . \( -path ./.git -o -path ./lua  -o -path ./system  -o -path ./android-project \) -prune -o \( -name '*.lua' -o -name '*.cpp' -o -name '*.h' \) -type f -print)
+gvim --cmd "set exrc" `git ls-files`
