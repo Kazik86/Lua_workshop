8620bc178c104de34409612a84538ef48fe4b299
mtrzeciak <michal.trzeciak@vivaldi.net>
2015-07-23 19:58:47 +0200

    'gFpsCounter' status overdraw

--- scripts/Main.lua
+++ scripts/Main.lua
@@ -2,15 +2,15 @@ Class = "Main"
 Super = _G.eLuaModuleMgr.derive("scripts/Actor.lua")
 
 function Init(me)
-    me.gFpsCounter = _G.gFpsCounter.create(me)
-    me.gFpsCounter:enable()
-
     me.background = _G.eActorMgr.add("scripts/background.lua")
 
     for i = 1, 20 do
         _G.eActorMgr.add("scripts/kutas.lua")
         _G.eActorMgr.add("scripts/bigKutas.lua")
     end
+
+    me.gFpsCounter = _G.gFpsCounter.create(me)
+    me.gFpsCounter:enable()
 end
 
 Super.DefState(This, {
