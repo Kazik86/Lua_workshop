eddff09d6e9879d6e261efad78fad81f17cf5a71
mtrzeciak <michal.trzeciak@vivaldi.net>
2015-12-12 22:46:47 +0100

    splash screen

--- scripts/Main.lua
+++ scripts/Main.lua
@@ -1,20 +1,59 @@
 Class = "Main"
 Super = _G.eLuaModuleMgr.derive("scripts/Actor.lua")
 
-function Init(me)
-    me.background = _G.eActorMgr.add("scripts/background.lua")
 
-    for i = 1, 50 do
-        _G.eActorMgr.add("scripts/kutas.lua")
-    --    _G.eActorMgr.add("scripts/bigKutas.lua")
-    end
+function Init(me)
+    me.splashScreen = _G.gTexture.create(me)
+    me.splashScreen:setName("textures/engine.png")
+    me.splashScreen:setPosFromActor(false)
+    me.splashScreen:setRotFromActor(false)
+    me.splashScreen:setSdlRectW(800)
+    me.splashScreen:setSdlRectH(480)
+    me.splashScreen:setSdlRectX(0) me.splashScreen:setSdlRectY(0)
+    me.splashScreen:setFadeDuration(1)
 
     me.gFpsCounter = _G.gFpsCounter.create(me)
     me.gFpsCounter:enable()
+
+    me.gTimer = _G.gTimer.create(me)
 end
 
+
 Super.DefState(This, {
-    Name = "state_main",
+    Name = "state_splashScreen",
+    Gadgets = { "gTimer", "splashScreen" },
+
+    Enter = function(me)
+	me.gTimer:start()
+    end,
+
+    Update = function(me)
+	if (me.gTimer:isElapsed(2000)) then
+	    return Shift(me, state_splashScreen_fadeOut)
+	end
+    end
 })
 
-EntryState = state_main
+
+Super.DefState(This, {
+    Name = "state_splashScreen_fadeOut",
+    Gadgets = { "splashScreen" },
+
+    Enter = function(me)
+	me.splashScreen:fadeOut()
+    end,
+
+    Update = function(me)
+	if (me.splashScreen:isFadeCompleted()) then
+	    return Shift(me, state_idle)
+	end
+    end
+})
+
+
+Super.DefState(This, {
+    Name = "state_idle"
+})
+
+
+EntryState = state_splashScreen
