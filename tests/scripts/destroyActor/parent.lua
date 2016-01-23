1212ba27a4bad489b49f9f2088dfdbaf6df77504
mtrzeciak <michal.trzeciak@vivaldi.net>
2016-01-23 16:50:10 +0100

    unit tests: make sure that destroyed actor is collected by GC

--- tests/scripts/destroyActor/parent.lua
+++ tests/scripts/destroyActor/parent.lua
@@ -9,7 +9,20 @@ function Init(me)
     DestroyActor(me, "child1")
     _G.assert(#me.Children == 2)
 
+    -- weak table
+    me.weak = { __mode = "kv" }
+    _G.setmetatable(me.weak, me.weak)
+    me.weak.child2 = me.child2
+
     DestroyActor(me, "child2")
     _G.assert(#me.Children == 1)
     _G.assert(me.child2 == nil)
+
+    -- when object referenced by value (or key) in a weak table is collected by
+    -- garbage collector, corresponding entry in the table is automatically
+    -- removed. (Programming in Lua, 3rd ed., 17.1)
+    -- This test makes me sure that destroyed actor was properly collected by GC.
+    _G.assert(me.weak.child2 ~= nil)
+    _G.collectgarbage()
+    _G.assert(me.weak.child2 == nil)
 end
